9d16c53cd3ed81e85a25c4adcdf4fdff2da43622
kartikay-bagla <kartikaybagla@gmail.com>
2023-08-14 14:44:14 +0530

    fix: added apschedular

--- scripts/cpu_ram_logger.py
+++ scripts/cpu_ram_logger.py
@@ -2,14 +2,13 @@ import datetime as dt
 import sqlite3
 import psutil
 import requests
-from value_logger.utils import every, getenv
+from value_logger.utils import getenv
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 SECRET_KEY = getenv("SECRET_KEY")
 DEVICE_NAME = getenv("DEVICE_NAME")
 DB_FILE_NAME = getenv("DB_FILE_NAME")
 API_URL = getenv("API_URL")
-
-db = sqlite3.connect(DB_FILE_NAME)
 
 
 def _init_table(db):
@@ -18,16 +17,19 @@ def _init_table(db):
     cursor.execute("CREATE TABLE IF NOT EXISTS last_executed (time TEXT)")
 
 
-def add_entry_to_db(db):
+def add_entry_to_db():
+    db = sqlite3.connect(DB_FILE_NAME)
     now = dt.datetime.now()
     cpu = psutil.cpu_percent()
     ram = psutil.virtual_memory().percent
+    print(f"Adding to DB: {now} {cpu} {ram}")
     cursor = db.cursor()
     cursor.execute("INSERT INTO metrics VALUES (?, ?, ?)", (now, cpu, ram))
     db.commit()
 
 
-def update_api(db):
+def update_api():
+    db = sqlite3.connect(DB_FILE_NAME)
     cursor = db.cursor()
     # get last updated
     cursor.execute("SELECT last_executed FROM last_executed limit 1")
@@ -59,6 +61,7 @@ def update_api(db):
                 "timestamp": row[0],
             }
         )
+    print(f"Pushing {len(metrics)} to API")
     # push to API
     resp = requests.post(API_URL, json=metrics, headers={"secret-key": SECRET_KEY})
     if resp.status_code != 200:
@@ -66,10 +69,16 @@ def update_api(db):
         print(resp.json())
         return
     # update last updated
+    print("Updating last executed.")
     cursor.execute("UPDATE last_executed SET last_executed = ?", (dt.datetime.now(),))
     db.commit()
 
 
+db = sqlite3.connect(DB_FILE_NAME)
 _init_table(db=db)
-every(5, add_entry_to_db, db=db)
-every(60, update_api, db=db)
+db.close()
+
+bs = BlockingScheduler()
+bs.add_job(add_entry_to_db, 'interval', seconds=5)
+bs.add_job(update_api, 'interval', minutes=1)
+bs.start()
